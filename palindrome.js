abc6c74c91057e030370d476e800658eb5c28513
Porson <porsonlee@gmail.com>
2020-03-18 21:57:49 -0700

    something was modified

--- palindrome.js
+++ palindrome.js
@@ -1,25 +1,33 @@
 // FUNCTION IMPLEMENTATION (MULTIPLE BUGS)
 const isPalindrome = function(str) {
   const noSpaces = str.split(" ").join("");
-  console.log("no spaces", noSpaces);
+  console.log("no spaces --> ", noSpaces);
   const midIndex = Math.floor(noSpaces.length/2);
+  console.log("mid index --> ", midIndex);
   const lastIndex = noSpaces.length - 1;
-
+  console.log("last index --> ", lastIndex);
   for (let i = 0; i < midIndex; i++) {
-    if (str[i] !== str[lastIndex - i]) return false;
-  }
-}
+    console.log("string --> ", str[i])
+    if (str[i] !== str[lastIndex - i]) {
+      return false;
+    }
+  } 
+    return true
+};
+
+
 
 // Assertion Function
 const assertPalindrome = function(word, expected) {
   console.log(`Testing isPalindrome(\"${word}\"):`);
   const actual = isPalindrome(word);
+  console.log("actual --> ", actual);
   if (actual === expected) {
     console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅ function returned ${actual}\n`);
   } else {
     console.log("\x1b[31m%s\x1b[0m", `\tFAIL 🛑 function returned ${actual} (expected ${expected})\n`);
   }
-}
+};
 
 
 // TEST CODE
